e798f1adca2f865d47eb9788ee14054255b35f64
Duminda <dumindags@gmail.com>
2019-05-26 12:39:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+import TourList from "./components/TourList";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <TourList />
+    </React.Fragment>
+  );
+}
+
+export default App;
